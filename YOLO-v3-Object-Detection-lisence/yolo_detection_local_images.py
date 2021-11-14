e4769ffa12e083f0cebf3125abe5f1971e6a31ca
mikekuanhsuan <91623101+mikekuanhsuan@users.noreply.github.com>
2021-11-14 10:15:23 +0800

    Update yolo_detection_local_images.py

--- YOLO-v3-Object-Detection-lisence/yolo_detection_local_images.py
+++ YOLO-v3-Object-Detection-lisence/yolo_detection_local_images.py
@@ -6,24 +6,6 @@ import shutil
 
 def detectlicense(img_path):
 
-    # image_url = img_path
-    # filename = image_url.split("/")[-1]
-    # # Open the url image, set stream to True, this will return the stream content.
-    # r = requests.get(image_url, stream = True)
-
-    # # Check if the image was retrieved successfully
-    # if r.status_code == 200:
-    # # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-    #     r.raw.decode_content = True
-
-    # # Open a local file with wb ( write binary ) permission.
-    #     with open('images/' + filename,'wb') as f:
-    #         shutil.copyfileobj(r.raw, f)
-    #         print('Image sucessfully Downloaded: ','images/'  + filename)
-    # else:
-    #     print('Image Could not be retreived')
-
-
     filename = img_path
 
     confidenceThreshold = 0.5
